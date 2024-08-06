43b18fc9b0a26f6f750e97d84c1c9ad8e5530af2
glitteryash <glitteryash@gmail.com>
2024-08-06 10:04:34 +0900

    model setting

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const Student = require("./models/student");
 const port = 3000;
 
 app.use(express.static("public"));
@@ -16,12 +17,18 @@ mongoose
   })
   .then(() => {
     console.log(`Successfully connected to mongoDB.`);
-  });
+  })
+  .catch((e) => {});
 
 app.get("/", (req, res) => {
   res.send("This is homepage");
 });
 
+app.get("/students/insert", (req, res) => {
+  res.render("studentInsert.ejs");
+});
+
+app.post("/students/insert", (req, res) => {});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
