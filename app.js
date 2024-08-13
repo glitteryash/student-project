e9c627602db79d4b6cf74514f908b77bba18bb64
glitteryash <glitteryash@gmail.com>
2024-08-13 12:02:50 +0900

    new update

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const bodyParser = require("body-parser");
 const Student = require("./models/student");
 const port = 3000;
 
+//middleware
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+//connect to mongoDB
 mongoose
   .connect("mongodb://localhost:27017/studentDB", {
     //   useNewUrlParser: true,
@@ -18,8 +20,11 @@ mongoose
   .then(() => {
     console.log(`Successfully connected to mongoDB.`);
   })
-  .catch((e) => {});
+  .catch((e) => {
+    console.error(`Failed to connected to mongoDB.` + e);
+  });
 
+// route setting
 app.get("/", (req, res) => {
   res.send("This is homepage");
 });
@@ -28,7 +33,50 @@ app.get("/students/insert", (req, res) => {
   res.render("studentInsert.ejs");
 });
 
-app.post("/students/insert", (req, res) => {});
+app.post("/students/insert", (req, res) => {
+  let { id, name, age, merit, other } = req.body;
+  const newStudent = new Student({
+    id,
+    name,
+    age,
+    scholarship: { merit, other },
+  });
+  newStudent
+    .save()
+    .then(() => {
+      console.log("Student is accepted.");
+      res.render("accept.ejs", { newStudent });
+    })
+    .catch((e) => {
+      console.error("Student is not accepted.", e);
+      res.render("reject.ejs");
+    });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.get("/students", async (req, res) => {
+  try {
+    let data = await Student.find();
+    res.render("students.ejs", { data });
+  } catch (e) {
+    console.error(`Failed to find data.`, e);
+    res.send(`Failed to find data.`);
+  }
+});
+
+app.get("/*", (req, res) => {
+  res.status(404);
+  res.send(`Not allowed.`);
+});
+
+//show current data from DB
+Student.find()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.error(e);
+  });
